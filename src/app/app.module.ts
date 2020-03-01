ce1f7aa4b68dde7c8f1c4dace6437818c9b90879
wcolts2000 <wcolts2000@gmail.com>
2020-03-01 00:50:12 -0800

    added manually a warning component with minimal styles and generated a success component and utilized bootstrap alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { WarningComponent } from "./components/warningComponent/warning.component";
+import { SuccessComponentComponent } from './components/success-component/success-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent, WarningComponent, SuccessComponentComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
