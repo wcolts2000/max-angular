72819eb63bc94346c7e8cc8151fcf7e1bb382cb2
wcolts2000 <wcolts2000@gmail.com>
2020-02-29 23:40:37 -0800

    example of using custom attribute as selector in server component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-servers",
   template: `
-    <app-server></app-server>
-    <app-server></app-server>
+    <div app-server></div>
+    <div app-server></div>
   `,
   styleUrls: ["./servers.component.css"]
 })
